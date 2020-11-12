849a935fca9782ced39ca71eb076e739ce77a779
Justin Brown <j-brown123@live.com>
2020-11-11 23:41:09 -0500

    fixed default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -6,6 +6,13 @@ const DefaultCss = styled.div`
 height: 90vh;
 color: white;
 letter-spacing: 1px;
+
+.img-class{  
+    max-width: 576px;
+    width: 80%;
+    min-width: 200px;
+    height: auto;
+}
 `
 
 
@@ -16,12 +23,15 @@ export default class Default extends Component {
 
             <div className="container">
                 <div className="row">
-                <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
-                    <h1>404</h1>
-                    <h1>Error:</h1>
-                    <h2 className="p-1">Page Not Found</h2>
-                    <h3 className="p-1">The Requested url <span className="text-danger">{this.props.location.pathname}</span>{" "}</h3>
-                    <h3 className="p-1">Was Not Found</h3>
+                <div className="mx-auto text-center text-uppercase pt-1">
+                    
+                    <h2 className="text-title">OOF Page not found</h2>
+                    <div><strong>404 Error</strong></div>
+
+                    <div className="pt-1">The Requested url <span className="text-danger">{this.props.location.pathname}</span>{" "}<br/>Was Not Found</div>
+                    <div className="pt-2">
+                        <img className="img-class" src="./images/GuyCrashing.jpg" alt="guy crashing"/>
+                    </div>
                     </div>
                 </div>
             </div>
